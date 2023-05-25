7845ab10833c4e88af203a922b037315c1c82e27
Waliyu Habeeb <Ademolawaliyu1@gmail.com>
2023-05-25 01:49:52 -0700

    first commit

---
+++ src/Header.js
@@ -0,0 +1,16 @@
+
+const Header = () => {
+  return (
+    <header>
+        <h1>Header</h1>
+        <h2>heloo hook</h2>
+        <input 
+        type="text"
+        
+         />
+      
+    </header>
+  )
+}
+
+export default Header;
